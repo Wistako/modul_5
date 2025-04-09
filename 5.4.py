203e445c804c900ef0329ed59409815783f6e683
Wistak <piotr.slomczynski@erpatech.com.pl>
2025-04-09 13:41:57 +0200

    Add generate fake movies and serial func

--- 5.4.py
+++ 5.4.py
@@ -1,3 +1,8 @@
+from faker import Faker
+import random
+
+fake = Faker()
+
 class Film():
     def __init__(self, title, year, genre, display_count):
         self.title = title
@@ -21,3 +26,36 @@ class Serial(Film):
 
     def __str__(self):
         return f'{self.title} S{self.sezon:02}E{self.episode:02}'
+
+films_serials = []    
+
+def generate_fake_movie(count):
+    arr = []
+    for i in range(count):
+        film = Film(
+            title=fake.catch_phrase(), 
+            year= random.randint(1950, 2025),
+            genre= random.choice(['Drama', 'Comedy', 'Action', 'Sci-Fi', "Romance", "Thriller"]),
+            display_count= random.randint(0, 10)
+            )
+        arr.append(film)
+    return arr
+
+def generate_fake_serial(count):
+    arr = []
+    for i in range(count):
+        film = Serial(
+            title=fake.catch_phrase(), 
+            year= random.randint(1950, 2025),
+            genre= random.choice(['Drama', 'Comedy', 'Action', 'Sci-Fi', "Romance", "Thriller"]),
+            display_count= random.randint(0, 10),
+            episode= random.randint(8,12),
+            sezon=1
+            )
+        arr.append(film)
+    return arr
+
+
+films_serials = films_serials + generate_fake_movie(5) + generate_fake_serial(5)
+
+print(films_serials)
